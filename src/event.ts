8a3533da7709b9d1b4a579f4fed91ff1ab98cacf
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-01-17 14:43:31 +0900

    sample editing

--- src/event.ts
+++ src/event.ts
@@ -1,14 +1,14 @@
 import { ItemStack } from "bdsx/bds/inventory";
+import { CompoundTag, NBT, Tag } from "bdsx/bds/nbt";
 import { MinecraftPacketIds } from "bdsx/bds/packetids";
 import { PlayerPermission } from "bdsx/bds/player";
 import { serverInstance } from "bdsx/bds/server";
 import { CANCEL } from "bdsx/common";
 import { events } from "bdsx/event";
 import { CustomTrade, VillagerInteractEvent } from "..";
-import { SNBT } from "@bdsx/snbt";
 import { Player$setCarriedItem } from "./hacker";
 
-const level = serverInstance.minecraft.getLevel();
+const MCLEVEL = serverInstance.minecraft.getLevel();
 
 const VILLAGER = "minecraft:villager_v2";
 
@@ -21,7 +21,7 @@ events
 
             const data = pkt.transaction;
 
-            const entity = level.getRuntimeEntity(data.runtimeId, false);
+            const entity = MCLEVEL.getRuntimeEntity(data.runtimeId, false);
             if (!entity) return;
             if (entity.getIdentifier() === VILLAGER) {
                 const event = new VillagerInteractEvent(
@@ -37,8 +37,61 @@ events
     });
 
 const AIR_ITEM = ItemStack.constructWith("minecraft:air", 1);
+events.serverLeave.on(() => {
+    AIR_ITEM.destruct();
+});
+
+function allocateRecipeTag(
+    buyItemA: ItemStack,
+    buyItemB: ItemStack | null,
+    destroy: boolean,
+    priceMultiplifer: number,
+    sellItem: ItemStack,
+    tier: number,
+    maxUses: number,
+    traderExp: number = -1
+): Tag {
+    if (tier > 4) tier = 4;
+
+    let rewardExp: boolean;
+    if (traderExp > 0) {
+        rewardExp = true;
+    } else {
+        rewardExp = false;
+        traderExp = 0;
+    }
+
+    const buyATag = buyItemA.save();
+    const sellTag = sellItem.save();
+
+    const retTag: any = {
+        buyA: buyATag,
+        buyACount: buyATag.Count,
+        buyBCount: NBT.int(0),
+        sell: sellTag,
+        demand: NBT.int(0),
+        maxUses: maxUses,
+        priceMultiplierA: priceMultiplifer,
+        rewardExp: rewardExp,
+        traderExp: traderExp,
+        uses: NBT.int(0),
+        tier: tier,
+    };
+    if (buyItemB) {
+        const buyBTag = buyItemB.save();
+        retTag.buyB = buyBTag;
+        retTag.buyBCount = buyBTag.Count;
+        destroy && buyItemB.destruct();
+    }
+    destroy && buyItemA.destruct();
+    destroy && sellItem.destruct();
+
+    return NBT.allocate(retTag);
+}
+
 CustomTrade.onVillagerInteract.on((ev) => {
     const player = ev.player;
+    const ni = player.getNetworkIdentifier();
     const villager = ev.villager;
     const item = ev.item;
     if (!CustomTrade.IsWand(item)) return;
@@ -46,14 +99,28 @@ CustomTrade.onVillagerInteract.on((ev) => {
         Player$setCarriedItem(player, AIR_ITEM);
         return;
     }
-    const OffersRecipes = villager.save().Offers.Recipes;
-    console.log(OffersRecipes);
+    // const OffersRecipes = villager.save().Offers.Recipes;
+    const recipe1 = allocateRecipeTag(
+        ItemStack.constructWith("minecraft:diamond", 3),
+        null,
+        true,
+        0,
+        ItemStack.constructWith("minecraft:emerald", 1),
+        0,
+        2147483647,
+        10
+    );
+    console.log(recipe1);
+
+    const villTag = villager.allocateAndSave();
+    const list = NBT.allocate([recipe1, recipe1]);
+    villTag.get<CompoundTag>("Offers")!.set("Recipes", list);
+    list.dispose();
+    villager.load(villTag);
+    villTag.dispose();
+
     return CANCEL;
 });
-events.serverLeave.on(() => {
-    AIR_ITEM.destruct();
-});
-
 /**
  * Offers . Recipes
  * {
