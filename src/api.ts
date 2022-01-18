738850539c7ae9e9cbcae817d11e91aff54633da
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-01-18 16:44:36 +0900

    Update API

--- src/api.ts
+++ src/api.ts
@@ -7,4 +7,16 @@ export namespace VillagerTradeAPI {
     export const removeAllRecipes = RecipesMgmt.removeAllRecipes;
     export const allocateRecipeTag = CustomTrade.allocateRecipeTag;
     export const onVillagerInteract = CustomTrade.onVillagerInteract;
+
+    export const RECIPE_MAX_TIER = CustomTrade.RECIPE_MAX_TIER;
+    export const RECIPE_DEFAULT_TIER = CustomTrade.RECIPE_DEFAULT_TIER;
+    export const RECIPE_MAX_USES = CustomTrade.RECIPE_MAX_USES;
+
+    export const RECIPE_DEFAULT_TRADER_EXP =
+        CustomTrade.RECIPE_DEFAULT_TRADER_EXP;
+
+    export const RECIPE_DEFAULT_PRICE_MULTIPLIER =
+        CustomTrade.RECIPE_DEFAULT_PRICE_MULTIPLIER;
+
+    export const RECIPE_DEFAULT_DEMAND = CustomTrade.RECIPE_DEFAULT_DEMAND;
 }
