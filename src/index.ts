bc12542347738d11393cb8cf75e2df604c0937e5
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-03-04 21:36:11 +0900

    sort `TierExpRequirements`

--- src/index.ts
+++ src/index.ts
@@ -91,23 +91,7 @@ export namespace TraderMgmt {
         const villTag = villager.save();
         villTag.Offers.Recipes.push(recipe);
 
-        villTag.Offers.TierExpRequirements = [
-            {
-                "0": 0,
-            },
-            {
-                "1": 10,
-            },
-            {
-                "2": 70,
-            },
-            {
-                "3": 150,
-            },
-            {
-                "4": 250,
-            },
-        ];
+        villTag.Offers.TierExpRequirements = [{ "0": 0 }, { "1": 10 }, { "2": 70 }, { "3": 150 }, { "4": 250 }];
 
         villager.load(villTag);
         recipe.dispose();
