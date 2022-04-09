1a2fea8218a84c34edef635b28b80cf09db91c1c
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-04-09 10:03:57 +0900

    remove using deprecated

--- src/event.ts
+++ src/event.ts
@@ -2,14 +2,13 @@ import { Mob } from "bdsx/bds/actor";
 import { ItemStack, ItemUseOnActorInventoryTransaction } from "bdsx/bds/inventory";
 import { MinecraftPacketIds } from "bdsx/bds/packetids";
 import { ServerPlayer } from "bdsx/bds/player";
-import { serverInstance } from "bdsx/bds/server";
 import { CANCEL } from "bdsx/common";
 import { events } from "bdsx/event";
+import { bedrockServer } from "bdsx/launcher";
 import { TraderMgmt } from ".";
 import { CustomTrade } from "..";
 
-const MCLEVEL = serverInstance.minecraft.getLevel();
-
+const mc_level = bedrockServer.level;
 export class VillagerInteractEvent {
     constructor(public player: ServerPlayer, public villager: Mob, public item: ItemStack, public transaction: ItemUseOnActorInventoryTransaction) {}
 }
@@ -21,7 +20,7 @@ events.packetBefore(MinecraftPacketIds.InventoryTransaction).on((pkt, ni, id) =>
 
         const data = pkt.transaction;
 
-        const entity = MCLEVEL.getRuntimeEntity(data.runtimeId, false);
+        const entity = mc_level.getRuntimeEntity(data.runtimeId, false);
         if (!entity) return;
         if (TraderMgmt.isValidTrader(entity)) {
             const event = new VillagerInteractEvent(player, entity, player.getMainhandSlot(), data);
